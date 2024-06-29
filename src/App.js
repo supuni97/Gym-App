60dde0e493483aac8756476929737bcab31fdf0d
supuni <sitharabandara1997@gmail.com>
2024-06-29 12:42:17 +0530

    navigation bar completed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <Box width="400px" sx={{width: {xl:'1488px'}}} m="auto" >
-    Navbar
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}  />
       <Route path="/exercise/:id" element={<ExerciseDetail />}  />
