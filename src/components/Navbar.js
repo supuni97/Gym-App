bbcfa37b1336b4bc274a6e24028d80302d17f704
supuni <sitharabandara1997@gmail.com>
2024-06-29 17:59:31 +0530

    Hero banner completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
       <Link to="/">
       <img src={Logo} alt="logo" style={{width:'48px', height:'48px', margin:'0 20px'}} />
       </Link>
-      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+      <Stack direction="row" gap="40px" fontSize="24px" fontFamily="'Poppins', sans-serif" alignItems="flex-end">
         <Link to="/" style={{textDecoration:'none',color:'#3A1212', borderBottom:'3px solid #FF2625'}}>Home</Link>
         <a href='#exercises' style={{textDecoration:'none',color:'#3A1212'}}>Exercises</a>
       </Stack>
