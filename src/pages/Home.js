a52581c31e96dfe9d7840fb395f57b1041f1cb2f
supuni <sitharabandara1997@gmail.com>
2024-06-30 19:54:13 +0530

    Updated searching function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,14 @@ import HeroBanner from '../components/HeroBanner';
 import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all');
+  const [exercises, setExercises] = useState([]);
+
   return (
     <Box>
       <HeroBanner/>
-      <SearchExercises/>
-      <Exercises/>
+      <SearchExercises  />
+      <Exercises />
     </Box>
   )
 }
