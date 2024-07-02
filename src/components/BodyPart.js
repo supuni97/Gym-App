a8a9d8178a2cb3abd1d7eb151fd712f8e73a154b
supuni <sitharabandara1997@gmail.com>
2024-07-02 18:24:53 +0530

    completed horizontal scroll bar

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -11,7 +11,10 @@ const BodyPart = ({ item, bodyPart, setBodyPart, imageUrl }) => {
       sx={bodyPart === item ? { 
         borderTop: '4px solid #FF2625', 
         background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
-      onClick={() => setBodyPart(item)}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({top:1800, left:100, behavior:'smooth' })
+        }}
     >
       <img src={imageUrl} alt={item} style={{ width: '80vw',
              height:'160px',
