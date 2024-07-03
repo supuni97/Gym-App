b9e17444fb85ae983ea761b6b4e9d460d96838e8
supuni <sitharabandara1997@gmail.com>
2024-07-03 12:14:34 +0530

    Made basic detail page

---
+++ src/components/Detail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Typography, Stack, Button } from '@mui/material';
+
+import BodyPartImage from '../assets/icons/body-part.png';
+import TargetImage from '../assets/icons/target.png';
+import EquipmentImage from '../assets/icons/equipment.png';
+
+const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
+  return (
+    <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }}>
+      <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+      <Stack sx={{ gap: { lg: '35px', xs: '220px' } }}>
+      <Typography sx={{ fontSize: { lg: '24px', xs: '18px' } }} color="#4F4C4C">
+          Exercises keep you strong.{' '}
+          <span style={{ textTransform: 'capitalize' }}>{name}</span> is one
+          of the best <br /> exercises to target your {target}. It will help you improve your{' '}
+          <br /> mood and gain energy.
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Detail;
---
+++ src/components/SimilarExersices.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SimilarExersices = () => {
+  return (
+    <div>SimilarExersices</div>
+  )
+}
+
+export default SimilarExersices
